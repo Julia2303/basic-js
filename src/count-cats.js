a6a25b3d8caa44f6cee14fe2246197b7e88f14a8
Julia2303 <julia.kazakova23@gmail.com>
2021-09-07 14:53:18 +0300

    feat: add solution tp carbon-dating.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -17,9 +17,6 @@ import { NotImplementedError } from '../extensions/index.js';
 export default function countCats(matrix) {
   let number = 0;
   for(let i = 0; i < matrix.length; i++) {
-    // for(let j = 0; j < matrix[i].length; j++) {
-    //   if(matrix[i][j] === '^^') number++;
-    // }
     number += matrix[i].filter(item => item === '^^').length;
   }
   return number;
